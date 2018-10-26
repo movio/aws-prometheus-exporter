dddce89a533071c0686087fe4edb8a892c29a820
Nicolas Maquet <nicolas@movio.co>
2018-10-26 15:02:15 +1300

    WIP

--- aws_metrics.py
+++ aws_metrics.py
@@ -10,6 +10,8 @@ import yaml
 import boto3
 from prometheus_client import REGISTRY, Gauge, start_http_server
 
+__all__ = ["AwsMetric", "AwsMetricCollectorThread", "parse_aws_metrics"]
+
 AwsMetric = namedtuple("AwsMetric", [
     "name",
     "description",
@@ -20,13 +22,14 @@ AwsMetric = namedtuple("AwsMetric", [
     "search"
 ])
 
-class MetricThread(threading.Thread):
+
+class AwsMetricCollectorThread(threading.Thread):
+
     def __init__(self, metric, registry, session):
         super().__init__()
         self.metric = metric
         self.registry = registry
         self.session = session
-
         self.gauge = None
 
     def _step(self):
@@ -37,7 +40,7 @@ class MetricThread(threading.Thread):
             value = resp.pop("value")
             assert list(resp.keys()) == keys
             gauge.labels(*resp.values()).set(value)
-            
+
     def _get_response_iterator(self):
         service = self.session.client(self.metric.service)
         paginator = service.get_paginator(self.metric.paginator)
@@ -47,52 +50,57 @@ class MetricThread(threading.Thread):
     def _get_gauge(self, name, description, labels):
         if self.gauge:
             return self.gauge
-        else:
-            gauge = Gauge(name, description, labels, registry=self.registry)
-            self.gauge = gauge
-            return gauge
+        # pylint: disable=unexpected-keyword-arg
+        gauge = Gauge(name, description, labels, registry=self.registry)
+        self.gauge = gauge
+        return gauge
 
     def run(self):
         while True:
             self._step()
             time.sleep(self.metric.update_freq_mins * 60)
 
-_VALID_METRIC_NAME_RE = re.compile("^[a-z_0-9]+$")
 
-def _parse_metrics(yaml_string):
-    obj = yaml.load(yaml_string)
-    ret = []
-    for metric_name, parsed_metric in obj.items():
-        if not _VALID_METRIC_NAME_RE.match(metric_name):
+VALID_METRIC_NAME_RE = re.compile("^[a-z_0-9]+$")
+
+
+def parse_aws_metrics(yaml_string):
+    parsed_yaml = yaml.load(yaml_string)
+    metrics = []
+
+    def get_field(field_name, metric_name, parsed_metric):
+        field_value = parsed_metric.get(field_name)
+        if field_value is None:
+            raise ValueError("metric '%s' is missing mandatory field '%s'" % (metric_name, field_name))
+        return field_value
+
+    for metric_name, parsed_metric in parsed_yaml.items():
+        if not VALID_METRIC_NAME_RE.match(metric_name):
             raise ValueError("metric name '%s' does not match [a-z_0-9]+" % metric_name)
-            
-        def get_field(field):
-            ret = parsed_metric.get(field)
-            if ret is None:
-                raise ValueError("metric '%s' is missing mandatory field '%s'" % (metric_name, field))
-            return ret
-            
-        ret.append(AwsMetric(
+
+        metrics.append(AwsMetric(
             name=metric_name,
-            description=get_field("description").strip(),
-            service=get_field("service").strip(),
-            paginator=get_field("paginator").strip(),
+            description=get_field("description", metric_name, parsed_metric).strip(),
+            service=get_field("service", metric_name, parsed_metric).strip(),
+            paginator=get_field("paginator", metric_name, parsed_metric).strip(),
             paginator_args=parsed_metric.get("paginator_args", {}),
             update_freq_mins=parsed_metric.get("update_freq_mins", 5),
-            search=get_field("search").strip()
+            search=get_field("search", metric_name, parsed_metric).strip()
         ))
 
-    return ret
+    return metrics
 
-if __name__ == "__main__":
-    with open(sys.argv[1]) as f:
-        cfg = f.read()
+
+def main():
+    # FIXME: add argparse
+    with open(sys.argv[1]) as metrics_file:
+        cfg = metrics_file.read()
     registry = REGISTRY
     session = boto3.session.Session()
 
-    metrics = _parse_metrics(cfg)
+    metrics = parse_aws_metrics(cfg)
     for metric in metrics:
-        thread = MetricThread(metric, registry, session)
+        thread = AwsMetricCollectorThread(metric, registry, session)
         thread.start()
 
     start_http_server(8000)
@@ -100,3 +108,7 @@ if __name__ == "__main__":
 
     while True:
         time.sleep(1000)
+
+
+if __name__ == "__main__":
+    main()
--- test/test_aws_metrics.py
+++ test/test_aws_metrics.py
@@ -3,7 +3,7 @@
 import unittest.mock as mock
 from collections import namedtuple
 
-from aws_metrics import _parse_metrics, AwsMetric
+from aws_metrics import parse_aws_metrics, AwsMetric, AwsMetricCollectorThread
 
 # pylint: disable=protected-access
 
@@ -48,7 +48,7 @@ ssm_agents_ec2_instance_ids:
 
 
 def test_load_single_aws_metric():
-    metrics = _parse_metrics(SINGLE_METRIC_YAML)
+    metrics = parse_aws_metrics(SINGLE_METRIC_YAML)
     assert len(metrics) == 1
     ec2_instance_ids = metrics[0]
     assert ec2_instance_ids == AwsMetric(
@@ -68,7 +68,7 @@ def test_load_single_aws_metric():
 
 
 def test_load_multiple_aws_metrics():
-    metrics = _parse_metrics(MULTIPLE_METRICS_YAML)
+    metrics = parse_aws_metrics(MULTIPLE_METRICS_YAML)
     assert len(metrics) == 2
     assert metrics[0] == AwsMetric(
         name="public_ec2_instance_ids",
@@ -76,10 +76,10 @@ def test_load_multiple_aws_metrics():
         service="ec2",
         paginator="describe_instances",
         paginator_args={
-          "Filters": [{
-            "Name": "instance-state-name",
-            "Values": ["Running"]
-          }]
+            "Filters": [{
+                "Name": "instance-state-name",
+                "Values": ["Running"]
+            }]
         },
         search="Reservations[].Instances[?PublicIpAddress].{id: InstanceId, value: `1`}[]",
         update_freq_mins=5
@@ -89,56 +89,60 @@ def test_load_multiple_aws_metrics():
         description="EC2 instance ids of instances with SSM agent",
         service="ssm",
         paginator_args={
-          "Filters": [
-            {
-                "Key": "ResourceType",
-                "Values": ["EC2Instance"]
-            },
-            {
-                "Key": "PingStatus",
-                "Values": ["Online"],
-            }
-          ]
+            "Filters": [
+                {
+                    "Key": "ResourceType",
+                    "Values": ["EC2Instance"]
+                },
+                {
+                    "Key": "PingStatus",
+                    "Values": ["Online"],
+                }
+            ]
         },
         paginator="describe_instance_information",
         search="InstanceInformationList[].{id: InstanceId, value: `1`}[]",
         update_freq_mins=5
     )
-# 
-# SessionMocks = namedtuple("SessionMocks", [
-#     "session",
-#     "service",
-#     "paginator",
-#     "paginate_response_iterator"
-# ])
-# 
-# 
-# def create_session_mocks(search_response_iterator):
-#     session = mock.NonCallableMagicMock()
-#     paginator = mock.NonCallableMagicMock()
-#     service = mock.NonCallableMagicMock()
-#     paginate_response_iterator = mock.NonCallableMagicMock()
-#     paginate_response_iterator.search = mock.Mock(return_value=search_response_iterator)
-#     paginator.paginate = mock.Mock(return_value=paginate_response_iterator)
-#     service.get_paginator = mock.Mock(return_value=paginator)
-#     session.client = mock.Mock(return_value=service)
-#     return SessionMocks(session, service, paginator, paginate_response_iterator)
-# 
-# 
-# def test_get_response_iterator():
-#     response_iterator = iter(["instance_id_1", "instance_id_2", "instance_id_3"])
-#     mocks = create_session_mocks(response_iterator)
-#     metrics_collector = AwsMetricsCollector(SINGLE_METRIC_YAML)
-#     assert response_iterator == metrics_collector._get_response_iterator(mocks.session, metrics_collector.metrics[0])
-#     mocks.session.client.assert_called_once_with("ec2")
-#     mocks.service.get_paginator.assert_called_once_with("describe_instances")
-#     mocks.paginator.paginate.assert_called_once_with(Filters=[{
-#         "Name": "instance-state-name",
-#         "Values": ["Running"]
-#     }])
-#     mocks.paginate_response_iterator.search.assert_called_once_with("Reservations[].Instances[].{id: InstanceId, value: `1`}[]")
-# 
-# 
+
+
+SessionMocks = namedtuple("SessionMocks", [
+    "session",
+    "service",
+    "paginator",
+    "paginate_response_iterator"
+])
+
+
+def create_session_mocks(search_response_iterator):
+    session = mock.NonCallableMagicMock()
+    paginator = mock.NonCallableMagicMock()
+    service = mock.NonCallableMagicMock()
+    paginate_response_iterator = mock.NonCallableMagicMock()
+    paginate_response_iterator.search = mock.Mock(return_value=search_response_iterator)
+    paginator.paginate = mock.Mock(return_value=paginate_response_iterator)
+    service.get_paginator = mock.Mock(return_value=paginator)
+    session.client = mock.Mock(return_value=service)
+    return SessionMocks(session, service, paginator, paginate_response_iterator)
+
+
+def test_get_response_iterator():
+    response_iterator = iter(["instance_id_1", "instance_id_2", "instance_id_3"])
+    mocks = create_session_mocks(response_iterator)
+    metrics = parse_aws_metrics(SINGLE_METRIC_YAML)
+    assert len(metrics) == 1
+    thread = AwsMetricCollectorThread(metric=metrics[0], registry=None, session=mocks.session)
+    assert response_iterator is thread._get_response_iterator()
+    mocks.session.client.assert_called_once_with("ec2")
+    mocks.service.get_paginator.assert_called_once_with("describe_instances")
+    mocks.paginator.paginate.assert_called_once_with(Filters=[{
+        "Name": "instance-state-name",
+        "Values": ["Running"]
+    }])
+    mocks.paginate_response_iterator.search.assert_called_once_with(
+        "Reservations[].Instances[].{id: InstanceId, value: `1`}[]")
+
+#
 # def test_run_metric():
 #     response_iterator = iter([
 #         { "id": "instance_id_1", "value": 1},
